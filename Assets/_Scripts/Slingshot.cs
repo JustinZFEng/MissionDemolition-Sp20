a0afb4c6690c1421930ca686f74627c1d329ae32
JustinZFEng <60244248+JustinZFEng@users.noreply.github.com>
2020-02-12 19:17:25 -0500

    Added tracking lines

--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
     [Header("Set in Inspector")]
 
     public GameObject prefabProjectile;
@@ -17,8 +18,18 @@ public class Slingshot : MonoBehaviour
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {                                                                                                                                                              
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
@@ -86,5 +97,6 @@ public class Slingshot : MonoBehaviour
             projectile = null;
         }
     }
-}
+}
+
 
